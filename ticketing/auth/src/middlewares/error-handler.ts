92655c85c1ee470fe56fa15fb37891d931d4344d
LaupWing <loc_strict@hotmail.com>
2021-04-14 11:22:25 +0200

    131

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import {Request, Response, NextFunction} from 'express'
+import {RequestValidationError} from '../errors/request-validation.error'
+import {DatabaseConnectionError} from '../errors/database-connection-error'
 
 export const errorHandler = (
    err: Error, 
@@ -6,7 +8,12 @@ export const errorHandler = (
    res: Response, 
    next: NextFunction
 )=>{
-   console.log('Something wen wrong', err)
+   if(err instanceof RequestValidationError){
+         
+   }
+   if(err instanceof DatabaseConnectionError){
+
+   }
 
    res.status(400).send({
       message: 'Something wen twrong'
