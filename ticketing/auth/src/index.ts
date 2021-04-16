404d3791940ce301739fe9c949b15437cdf8fad2
LaupWing <loc_strict@hotmail.com>
2021-04-16 15:31:47 +0200

    136

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -5,6 +5,7 @@ import signinRouter from './routes/signin'
 import signoutRouter from './routes/signout'
 import signupRouter from './routes/signup'
 import {errorHandler} from './middlewares/error-handler'
+import {NotFoundError} from './errors/not-found-error'
 
 const app = express()
 
@@ -14,6 +15,9 @@ app
    .use(signinRouter)
    .use(signoutRouter)
    .use(signupRouter)
+   .get('*', ()=>{
+      throw new NotFoundError()
+   })
    .use(errorHandler)
    .listen(3000, ()=>{
       console.log('[Auth] Listening on port 3000!')
