8c0f7f4c896b797d84e64e151e09ddf7fa2c4f21
LaupWing <loc_strict@hotmail.com>
2021-04-20 14:31:13 +0200

    164

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response} from 'express'
 import { body, validationResult } from 'express-validator'
 import {RequestValidationError} from '../errors/request-validation.error'
-import {DatabaseConnectionError} from '../errors/database-connection-error'
+import jwt from 'jsonwebtoken'
 import BadRequestError from '../errors/bad-request-error'
 import {User} from '../models/user'
 
@@ -29,7 +29,14 @@ router.post('/api/users/signup', [
 
    const user = User.build({email, password})
    await user.save()
+   const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+   }, 'secret')
 
+   req.session = {
+      jwt: userJwt
+   }
    res.status(201).send(user)
 })
 
