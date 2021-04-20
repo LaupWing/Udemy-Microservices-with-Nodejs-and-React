2fbd5f756825e5b388f5d0d4732ed06f3fa8e32b
LaupWing <loc_strict@hotmail.com>
2021-04-20 14:13:29 +0200

    163

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import {json} from 'body-parser'
 import 'express-async-errors'
 import mongoose from 'mongoose'
+import cookieSession from 'cookie-session'
 
 import currentUserRouter from './routes/current-user'
 import signinRouter from './routes/signin'
@@ -14,7 +15,12 @@ import {NotFoundError} from './errors/not-found-error'
 const app = express()
 
 app
+   .set('trust proxy', true)
    .use(json())
+   .use(cookieSession({
+      signed: false,
+      secure: true
+   }))
    .use(currentUserRouter)
    .use(signinRouter)
    .use(signoutRouter)
