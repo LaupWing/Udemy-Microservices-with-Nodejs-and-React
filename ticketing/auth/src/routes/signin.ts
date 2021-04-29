6d2f16f40ba016ef88dd611b000dbb980b619b59
LaupWing <loc_strict@hotmail.com>
2021-04-29 12:29:13 +0200

    173

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,25 +1,23 @@
 import express, { Request, Response } from 'express'
-import { body, validationResult } from 'express-validator'
-import {RequestValidationError} from '../errors/request-validation.error'
+import { body} from 'express-validator'
+import {validateRequest} from '../middlewares/valid-request'
 
 const router = express.Router()
 
 router
    .post('/api/users/signin', [
-      body('email')
-         .isEmail()
-         .withMessage('Email must be valid'),
-      body('password')
-         .trim()
-         .notEmpty()
-         .withMessage('You must supply a password')
-   ],
+         body('email')
+            .isEmail()
+            .withMessage('Email must be valid'),
+         body('password')
+            .trim()
+            .notEmpty()
+            .withMessage('You must supply a password')
+      ],
+      validateRequest,
       (req: Request, res: Response) => {
-         const errors = validationResult(req)
-         if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array())
-         }
-         res.send('Hi there!')
-      })
+         
+      }
+   )
 
 export default router
