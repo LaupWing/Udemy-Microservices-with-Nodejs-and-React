35ccd50086571f6c31b290833da28e879027aa4d
LaupWing <loc_strict@hotmail.com>
2021-05-01 14:58:38 +0200

    185

---
+++ ticketing/auth/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import {json} from 'body-parser'
+import 'express-async-errors'
+import mongoose from 'mongoose'
+import cookieSession from 'cookie-session'
+
+import currentUserRouter from './routes/current-user'
+import signinRouter from './routes/signin'
+import signoutRouter from './routes/signout'
+import signupRouter from './routes/signup'
+
+import {errorHandler} from './middlewares/error-handler'
+import {NotFoundError} from './errors/not-found-error'
+
+const app = express()
+
+app
+   .set('trust proxy', true)
+   .use(json())
+   .use(cookieSession({
+      signed: false,
+      secure: true
+   }))
+   .use(currentUserRouter)
+   .use(signinRouter)
+   .use(signoutRouter)
+   .use(signupRouter)
+   .use(errorHandler)
+   .all('*', ()=>{
+      throw new NotFoundError()
+   })
+   
+export {app}
--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,35 +1,6 @@
-import express from 'express'
-import {json} from 'body-parser'
-import 'express-async-errors'
 import mongoose from 'mongoose'
-import cookieSession from 'cookie-session'
+import {app} from './app'
 
-import currentUserRouter from './routes/current-user'
-import signinRouter from './routes/signin'
-import signoutRouter from './routes/signout'
-import signupRouter from './routes/signup'
-
-import {errorHandler} from './middlewares/error-handler'
-import {NotFoundError} from './errors/not-found-error'
-
-const app = express()
-
-app
-   .set('trust proxy', true)
-   .use(json())
-   .use(cookieSession({
-      signed: false,
-      secure: true
-   }))
-   .use(currentUserRouter)
-   .use(signinRouter)
-   .use(signoutRouter)
-   .use(signupRouter)
-   .use(errorHandler)
-   .all('*', ()=>{
-      throw new NotFoundError()
-   })
-   
 const start = async () =>{
    if(!process.env.JWT_KEY){
       throw new Error('No key')
