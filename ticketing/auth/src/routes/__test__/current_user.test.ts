e3b85f97646a6d1e9102092da99a10c2e2646d3e
LaupWing <loc_strict@hotmail.com>
2021-05-04 12:15:23 +0200

    196

--- ticketing/auth/src/routes/__test__/current_user.test.ts
+++ ticketing/auth/src/routes/__test__/current_user.test.ts
@@ -2,17 +2,19 @@ import request from 'supertest'
 import {app} from '../../app'
 
 it('responds with details about current user', async ()=>{
-   await request(app)
+   const authRes = await request(app)
       .post('/api/users/signup')
       .send({
          email: 'test@test.com',
          password:'password'
       })
       .expect(201)
+   const cookie = authRes.get('Set-Cookie')
 
    const response = await request(app)
       .get('/api/users/currentuser')
+      .set('Cookie', cookie)
       .send()
       .expect(200)
-   
+   expect(response.body.currentUser.email).toEqual('test@test.com')
 })
