677ec57a66ecb2c42df303c66dd3c2bc56c44417
LaupWing <loc_strict@hotmail.com>
2021-05-05 15:20:09 +0200

    207

--- ticketing/client/pages/auth/signup.js
+++ ticketing/client/pages/auth/signup.js
@@ -1,16 +1,38 @@
-export default () =>(
-   <form>
-      <h1>Signup</h1>
-      <div className="form-group">
-         <label>Email Adress</label>
-         <input type="text" className="form-control"/>
-      </div>
-      <div className="form-group">
-         <label>Password</label>
-         <input type="password" className="form-control"/>
-      </div>
-      <button className="btn btn-primary">
-         Sign up
-      </button>
-   </form>
-)
+import {useState} from 'react'
+
+export default () =>{
+   const [email, setEmail] = useState('') 
+   const [password, setPassword] = useState('') 
+
+   const onSubmit = e =>{
+      e.preventDefault()
+      console.log({email,password})
+   }
+
+   return (
+      <form onSubmit={onSubmit}>
+         <h1>Signup</h1>
+         <div className="form-group">
+            <label>Email Adress</label>
+            <input 
+               type="text" 
+               className="form-control"
+               value={email}
+               onChange={e => setEmail(e.target.value)}
+               />
+         </div>
+         <div className="form-group">
+            <label>Password</label>
+            <input 
+               type="password" 
+               className="form-control"
+               value={password}
+               onChange={e => setPassword(e.target.value)}
+            />
+         </div>
+         <button className="btn btn-primary">
+            Sign up
+         </button>
+      </form>
+   )
+}
