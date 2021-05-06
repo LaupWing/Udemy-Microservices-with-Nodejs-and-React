cdaef31d429692c601b888edba3e3d422ff72376
LaupWing <loc_strict@hotmail.com>
2021-05-06 15:36:44 +0200

    225

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,14 +1,12 @@
 import buildClient from '../api/buildClient'
 
 const LandingPage = ({currentUser}) =>{
-   console.log(currentUser)
-   return (
-      <h1>Landing page</h1>
-   )
+   return currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>
 }
 
 LandingPage.getIntialProps = async ({req}) =>{
    const {data} = await buildClient(req).get('/api/users/currentuser')
+   return data
 }
 
 export default LandingPage
