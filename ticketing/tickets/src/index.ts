aad520bc8872ea5aaa60d4acabc9da6100e543ff
LaupWing <loc_strict@hotmail.com>
2021-05-11 13:42:49 +0200

    248

--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -5,6 +5,9 @@ const start = async () =>{
    if(!process.env.JWT_KEY){
       throw new Error('No key')
    }
+   if(!process.env.MONGO_URI){
+      throw new Error('MONGO_URI must be defined')
+   }
    try{
       await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
          useNewUrlParser: true,
