4d4cebaadfff1cee3114dc31fecde341f4b572b0
LaupWing <loc_strict@hotmail.com>
2021-05-11 12:32:27 +0200

    246

---
+++ ticketing/tickets/src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express'
+import {json} from 'body-parser'
+import 'express-async-errors'
+import mongoose from 'mongoose'
+import cookieSession from 'cookie-session'
+
+import {errorHandler} from '@ticketservice/common'
+import {NotFoundError} from '@ticketservice/common'
+
+const app = express()
+
+app
+   .set('trust proxy', true)
+   .use(json())
+   .use(cookieSession({
+      signed: false,
+      secure: process.env.NODE_ENV !== 'test'
+   }))
+   .use(errorHandler)
+   .all('*', ()=>{
+      throw new NotFoundError()
+   })
+   
+export {app}
---
+++ ticketing/tickets/src/index.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+import {app} from './app'
+
+const start = async () =>{
+   if(!process.env.JWT_KEY){
+      throw new Error('No key')
+   }
+   try{
+      await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+         useCreateIndex: true
+      })
+   }catch(err){
+      console.log(err)
+   }
+   app.listen(3000, ()=>{
+      console.log('[Auth] Listening on port 3000!')
+   })
+}
+start()
