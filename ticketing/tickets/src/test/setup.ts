ec375bde045ae5e8678f3cbd3bc157b516fc7b0b
LaupWing <loc_strict@hotmail.com>
2021-05-12 13:21:00 +0200

    256

--- ticketing/tickets/src/test/setup.ts
+++ ticketing/tickets/src/test/setup.ts
@@ -36,17 +36,5 @@ afterAll(async ()=>{
 })
 
 global.signin = async () =>{
-   const email = 'test@test.com'
-   const password = 'password'
-
-   const response = await request(app)
-      .post('/api/users/signup')
-      .send({
-         email,
-         password
-      })
-      .expect(201)
-
-   const cookie = response.get('Set-Cookie')
-   return cookie
+   
 }
