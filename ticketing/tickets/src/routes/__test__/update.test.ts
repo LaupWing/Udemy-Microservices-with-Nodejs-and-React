4d7678bc756cb0342fc19757fb236fb9b525b067
LaupWing <loc_strict@hotmail.com>
2021-05-15 13:26:35 +0200

    269

---
+++ ticketing/tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,52 @@
+import request from 'supertest'
+import {app} from '../../app'
+import mongoose from 'mongoose'
+
+it('returns a 404 if the provided id doest not exists', async ()=>{
+   const id = mongoose.Types.ObjectId().toHexString()
+
+   await request(app)
+      .put(`/api/tickets/${id}`)
+      .set('Cookie', global.signin())
+      .send({
+         title: 'werwerwe',
+         price: 20
+      })
+      .expect(404)
+})
+it('returns a 401 if the user is not authenticated', async ()=>{
+   const id = mongoose.Types.ObjectId().toHexString()
+
+   await request(app)
+      .put(`/api/tickets/${id}`)
+      .send({
+         title: 'werwerwe',
+         price: 20
+      })
+      .expect(401)
+
+})
+it('returns a 401 if the does not own the ticket', async ()=>{
+   const response = await request(app)
+      .get('/api/tickets')
+      .send()
+      .expect(200)
+
+   expect(response.body.length).toEqual(3)
+})
+it('returns a 400 if the user provides an invalid title or price', async ()=>{
+   const response = await request(app)
+      .get('/api/tickets')
+      .send()
+      .expect(200)
+
+   expect(response.body.length).toEqual(3)
+})
+it('updates the ticket provided valid inputs', async ()=>{
+   const response = await request(app)
+      .get('/api/tickets')
+      .send()
+      .expect(200)
+
+   expect(response.body.length).toEqual(3)
+})
