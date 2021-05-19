6d1ec089f51b916468001b7e0a6bcb172ed211b9
LaupWing <loc_strict@hotmail.com>
2021-05-19 11:44:42 +0200

    298

--- ticketing/nats-test/src/listener.ts
+++ ticketing/nats-test/src/listener.ts
@@ -15,27 +15,7 @@ stan.on('connect', ()=>{
       process.exit()
    })
 
-   const options = stan
-      .subscriptionOptions()
-      .setManualAckMode(true)
-      .setDeliverAllAvailable()
-      .setDurableName('accouting-service')
-      
-   const subscription = stan.subscribe(
-      'ticket:created', 
-      'order-service-queue-group',
-      options
-   )
-
-   subscription.on('message', (msg: Message)=>{
-      const data = msg.getData()
-
-      if(typeof data === 'string'){
-         console.log(`Received event #${msg.getSequence()} data ${data}`)
-      }
-
-      msg.ack()
-   })
+   new TicketCreatedListener(stan).listen()
 })
 
 process.on('SIGINT', ()=>stan.close())
@@ -85,5 +65,16 @@ abstract class Listener{
          ? JSON.parse(data) 
          : JSON.parse(data.toString('utf8'))
 
+   }
+}
+
+class TicketCreatedListener extends Listener{
+   subject = 'ticket:created'
+   queueGroupName = 'payments-service'
+
+   onMessage(data: any, msg: Message){
+      console.log(`Event data ${data}`)
+
+      msg.ack()
    }
 }
