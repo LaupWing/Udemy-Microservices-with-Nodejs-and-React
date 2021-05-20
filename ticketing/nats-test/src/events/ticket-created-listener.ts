2555748f95d026a408202919727abc086259f271
LaupWing <loc_strict@hotmail.com>
2021-05-20 10:46:50 +0200

    307

--- ticketing/nats-test/src/events/ticket-created-listener.ts
+++ ticketing/nats-test/src/events/ticket-created-listener.ts
@@ -8,8 +8,10 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent>{
    readonly subject: Subjects.TicketCreated = Subjects.TicketCreated
    queueGroupName = 'payments-service'
 
-   onMessage(data: any, msg: Message){
-      console.log(`Event data ${data}`)
+   onMessage(data: TicketCreatedEvent['data'], msg: Message){
+      console.log(`Event data ${data.id}`)
+      console.log(`Event data ${data.title}`)
+      console.log(`Event data ${data.price}`)
 
       msg.ack()
    }
