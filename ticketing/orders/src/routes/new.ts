874b175465949d8390ff233de210f9c42c774529
LaupWing <loc_strict@hotmail.com>
2021-05-23 14:24:42 +0200

    337

--- ticketing/orders/src/routes/new.ts
+++ ticketing/orders/src/routes/new.ts
@@ -1,8 +1,17 @@
 import express, {Request, Response} from 'express'
+import {requireAuth, validateRequest} from '@ticketservice/common'
+import { body } from 'express-validator'
+import mongoose from 'mongoose'
 
 const router = express.Router()
 
-router.post('/api/orders', async (req: Request, rest: Response)=>{
+router.post('/api/orders', requireAuth, [
+   body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string)=> mongoose.Types.ObjectId.isValid(input))
+      .withMessage('Ticketid must be provided')
+], validateRequest, async (req: Request, rest: Response)=>{
 
 })
 
