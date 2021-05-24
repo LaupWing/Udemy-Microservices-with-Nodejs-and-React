5d679f07592397373c6dacdc53b691e618b69715
LaupWing <loc_strict@hotmail.com>
2021-05-24 14:03:16 +0200

    351

--- ticketing/orders/src/routes/__test__/new.test.ts
+++ ticketing/orders/src/routes/__test__/new.test.ts
@@ -37,5 +37,15 @@ it('returns an error if the ticket is already reserved', async ()=>{
 })
 
 it('reserves a ticket', async ()=>{
+   const ticket = Ticket.build({
+      title: 'concert',
+      price: 20
+   })
 
+   ticket.save()
+   await request(app)
+      .post('/api/orders')
+      .set('Cookie', global.signin())
+      .send({ticketId: ticket.id})
+      .expect(201)
 })
