b2efde401d4916aec3f16d89d97175943537eadd
LaupWing <loc_strict@hotmail.com>
2021-05-24 14:01:02 +0200

    350

--- ticketing/orders/src/routes/__test__/new.test.ts
+++ ticketing/orders/src/routes/__test__/new.test.ts
@@ -1,6 +1,8 @@
 import request from 'supertest'
 import { app } from '../../app'
 import mongoose from 'mongoose'
+import { Ticket } from '../../models/ticket'
+import { Order, OrderStatus } from '../../models/order'
 
 it('returns an error if the ticket doest not exist', async ()=>{
    const ticketId = mongoose.Types.ObjectId()
@@ -13,7 +15,25 @@ it('returns an error if the ticket doest not exist', async ()=>{
 })
 
 it('returns an error if the ticket is already reserved', async ()=>{
+   const ticket = Ticket.build({
+      title: 'concert',
+      price: 20
+   })
+   await ticket.save()
+   const order = Order.build({
+      ticket,
+      userId: '2134124',
+      status: OrderStatus.Created,
+      expiresAt: new Date()
+   })
 
+   await order.save()
+
+   await request(app)
+      .post('/api/orders')
+      .set('Cookie', global.signin())
+      .send({ticketId: ticket.id})
+      .expect(400)
 })
 
 it('reserves a ticket', async ()=>{
