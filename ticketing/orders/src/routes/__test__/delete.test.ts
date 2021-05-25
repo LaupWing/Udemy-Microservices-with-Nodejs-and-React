3e2c5c460da69a2a7ba332cb292ee7b8ba130f67
LaupWing <loc_strict@hotmail.com>
2021-05-25 12:16:31 +0200

    363

--- ticketing/orders/src/routes/__test__/delete.test.ts
+++ ticketing/orders/src/routes/__test__/delete.test.ts
@@ -1,4 +1,5 @@
 import request from 'supertest'
+import { natsWrapper } from '../../../__mocks__/nats-wrapper'
 import { app } from '../../app'
 import { Order, OrderStatus } from '../../models/order'
 import { Ticket } from '../../models/ticket'
@@ -27,4 +28,25 @@ it('marks an order as cancelled', async ()=>{
    expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled)
 })
 
-it.todo('emits a order cancelled event')
+it('emits a order cancelled event', async ()=>{
+   const ticket = Ticket.build({
+      title: 'concert',
+      price: 20
+   })
+
+   await ticket.save()
+
+   const user = global.signin()
+   const {body:order} = await request(app)
+      .post('/api/orders')
+      .set('Cookie', user)
+      .send({ticketId: ticket.id})
+      .expect(201)
+   
+   await request(app)
+      .delete(`/api/orders/${order.id}`)
+      .send()
+      .expect(204)
+   
+   expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
