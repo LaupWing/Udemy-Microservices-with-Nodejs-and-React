f6ef8ee5b1810b6cb18da85a3f92ee272944498a
LaupWing <loc_strict@hotmail.com>
2021-05-26 13:58:45 +0200

    378

--- ticketing/tickets/src/models/__test__/test.test.ts
+++ ticketing/tickets/src/models/__test__/test.test.ts
@@ -1,5 +1,24 @@
 import { Ticket } from "../ticket";
 
-it('implements optimistic concurrency control', async()=>{
-   
+it('implements optimistic concurrency control', async(done)=>{
+   const ticket = Ticket.build({
+      title: 'concert',
+      price: 5,
+      userId: '123'
+   })
+   await ticket.save()
+
+   const firstInstance = await Ticket.findById(ticket.id)
+   const secondInstance = await Ticket.findById(ticket.id)
+
+   firstInstance!.set({price: 10})
+   secondInstance!.set({price: 15})
+
+   await firstInstance!.save()
+   try{
+      await secondInstance!.save()
+   }catch(err){
+      return done()
+   }
+   throw new Error('Should not reacht this point')
 })
