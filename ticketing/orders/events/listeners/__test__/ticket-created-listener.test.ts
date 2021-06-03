afd69f903a8a26240fcca94f06bdc9c78dd1045d
LaupWing <loc_strict@hotmail.com>
2021-06-03 15:00:42 +0200

    388

--- ticketing/orders/events/listeners/__test__/ticket-created-listener.test.ts
+++ ticketing/orders/events/listeners/__test__/ticket-created-listener.test.ts
@@ -1,6 +1,38 @@
+import mongoose from 'mongoose'
+import { TicketUpdatedEvent } from "@ticketservice/common"
+import { natsWrapper } from "../../../__mocks__/nats-wrapper"
+import { TicketCreatedListener } from "../ticket-created-listener"
+import { Ticket } from '../../../src/models/ticket'
+
+const setup = async ()=>{
+   // @ts-ignore
+   const listener = new TicketCreatedListener(natsWrapper.client)
+   const data: TicketUpdatedEvent['data'] = {
+      version: 0,
+      id: new mongoose.Types.ObjectId().toHexString(),
+      title: 'concert',
+      price: 10,
+      userId: new mongoose.Types.ObjectId().toHexString()   
+   }
+   // @ts-ignore
+   const msg: Message = {
+      ack: jest.fn()
+   }
+   return {
+      listener,
+      data,
+      msg
+   }
+}
 
 it('creates and saves a ticket', async ()=>{
-   
+   const {listener, msg, data} = await setup()
+
+   await listener.onMessage(data, msg)
+   const ticket = await Ticket.findById(data.id)
+   expect(ticket).toBeDefined()
+   expect(ticket!.title).toEqual(data.title)
+   expect(ticket!.price).toEqual(data.price)
 })
 
 it('acks the message', async ()=>{
