3b87f3dd2f098827ea5e0eeafc548056a2e26832
LaupWing <loc_strict@hotmail.com>
2021-06-04 14:15:38 +0200

    391

--- ticketing/orders/events/listeners/__test__/ticket-updated-listener.test.ts
+++ ticketing/orders/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -36,8 +36,19 @@ const setup = async ()=>{
 }
 
 it('finds, updates, and saves a ticket', async ()=>{
-   
+   const {msg, data, ticket, listener} = await setup()
+
+   await listener.onMessage(data, msg)
+
+   const updatedTicket = await Ticket.findById(ticket.id)
+
+   expect(updatedTicket!.title).toEqual(data.title)
+   expect(updatedTicket!.price).toEqual(data.price)
+   expect(updatedTicket!.version).toEqual(data.version)
 })
 it('ack the message', async ()=>{
+   const {data, listener, msg} = await setup()
 
+   await listener.onMessage(data, msg)
+   expect(msg.ack).toHaveBeenCalled()
 })
