dac3a92abc3d0a30df5dcc1b3d6c9528feeeefef
LaupWing <loc_strict@hotmail.com>
2021-06-05 10:30:58 +0200

    396

---
+++ ticketing/tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,14 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@ticketservice/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queueGroupName";
+
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+   subject: Subjects.OrderCreated = Subjects.OrderCreated
+
+   queueGroupName = queueGroupName
+
+   async onMessage(data: OrderCreatedEvent['data'], msg: Message){
+      
+   }
+}
