340758c185d79bd590700e0cb7eec7cd260a4271
LaupWing <loc_strict@hotmail.com>
2021-06-05 10:44:29 +0200

    398

--- ticketing/tickets/src/events/listeners/order-created-listener.ts
+++ ticketing/tickets/src/events/listeners/order-created-listener.ts
@@ -1,5 +1,6 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@ticketservice/common";
 import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
 import { queueGroupName } from "./queueGroupName";
 
 
@@ -9,6 +10,16 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
    queueGroupName = queueGroupName
 
    async onMessage(data: OrderCreatedEvent['data'], msg: Message){
-      
+      const ticket = await Ticket.findById(data.ticket.id)
+
+      if(!ticket){
+         throw new Error('Ticket not found')
+      }
+
+      ticket.set({orderId: data.id})
+
+      await ticket.save()
+
+      msg.ack()
    }
 }
