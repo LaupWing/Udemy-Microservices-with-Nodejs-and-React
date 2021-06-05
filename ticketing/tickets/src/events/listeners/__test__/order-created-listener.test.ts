56f26e97f82620db0d7292a4d7933ec63922aec6
LaupWing <loc_strict@hotmail.com>
2021-06-05 15:05:48 +0200

    400

--- ticketing/tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ ticketing/tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -50,3 +50,11 @@ it('sets the userId of the ticket', async()=>{
 
    expect(updatedTicket!.orderId).toEqual(data.id)
 })
+
+it('acks the message', async()=>{
+   const {listener, data, msg} = await setup()
+
+   await listener.onMessage(data, msg)
+
+   expect(msg.ack).toBeCalled()
+})
