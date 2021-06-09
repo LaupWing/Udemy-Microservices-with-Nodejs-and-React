79bc1e4cffba79a122958de5802dacbe1da60df4
LaupWing <loc_strict@hotmail.com>
2021-06-09 12:03:43 +0200

    423

--- ticketing/orders/events/listeners/expiration-complete-listener.ts
+++ ticketing/orders/events/listeners/expiration-complete-listener.ts
@@ -1,6 +1,7 @@
 import { ExpirationCompleteEvent, Listener, OrderStatus, Subjects } from "@ticketservice/common";
 import { Message } from "node-nats-streaming";
 import { Order } from "../../src/models/order";
+import { OrderCancelledPublisher } from "../publisher/order-cancelled-publisher";
 import { queueGroupName } from "./queue-group-name";
 
 
@@ -9,15 +10,25 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
    subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete
 
    async onMessage(data: ExpirationCompleteEvent['data'], msg: Message){
-      const order = await Order.findById(data.orderId)
+      const order = await Order.findById(data.orderId).populate('ticket')
 
       if(!order){
          throw new Error('Order not found')
       }
 
       order.set({
-         status: OrderStatus.Cancelled,
-         ticket: null
+         status: OrderStatus.Cancelled
       })
+
+      await order.save()
+      await new OrderCancelledPublisher(this.client).publish({
+         id: order.id,
+         version: order.version,
+         ticket:{
+            id: order.ticket.id
+         }
+      })
+
+      msg.ack()
    }
 }
