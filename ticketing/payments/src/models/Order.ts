4e1a5209cf4fa8750ae5296b6638adb7e7984b12
LaupWing <loc_strict@hotmail.com>
2021-06-10 17:53:10 +0200

    431

--- ticketing/payments/src/models/Order.ts
+++ ticketing/payments/src/models/Order.ts
@@ -1,3 +1,4 @@
+import { OrderStatus } from '@ticketservice/common';
 import mongoose from 'mongoose'
 
 interface OrderAttrs {
@@ -5,7 +6,52 @@ interface OrderAttrs {
    version: number
    userId: string
    price: number
-   status: 
+   status: OrderStatus
 }
 
-interface OrderDoc extends mongoose.Do
+interface OrderDoc extends mongoose.Document{
+   version: number
+   userId: string
+   price: number
+   status: OrderStatus
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc>{
+   build(attrs: OrderStatus): OrderDoc
+}
+
+const orderSchema = new mongoose.Schema({
+   userId:{
+      type: String,
+      required: true
+   },
+   price: {
+      type: Number,
+      required: true
+   },
+   status:{
+      type: String,
+      required: true
+   }
+},{
+   toJSON:{
+      transform(doc, ret){
+         ret.id = ret._id
+         delete ret._id
+      }
+   }
+})
+
+orderSchema.statics.build = (attrs: OrderAttrs) =>{
+   return new Order({
+      _id: attrs.id,
+      version: attrs.version,
+      price: attrs.price,
+      userId: attrs.userId,
+      status: attrs.status,
+   })
+}
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema)
+
+export {Order}
