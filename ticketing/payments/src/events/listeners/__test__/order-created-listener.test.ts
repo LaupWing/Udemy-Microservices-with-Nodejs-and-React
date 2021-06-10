daecbf93838ee5c0f9985747f9fc0eee18c9e2a4
LaupWing <loc_strict@hotmail.com>
2021-06-10 22:42:06 +0200

    434

--- ticketing/payments/src/events/listeners/__test__/order-created-listener.test.ts
+++ ticketing/payments/src/events/listeners/__test__/order-created-listener.test.ts
@@ -1,7 +1,8 @@
-import { OrderCreatedEvent } from "@ticketservice/common"
+import { OrderCreatedEvent, OrderStatus } from "@ticketservice/common"
 import { natsWrapper } from "../../../nats-wrapper"
 import { OrderCreatedListener } from "../order-created-listener"
 import mongoose from 'mongoose'
+import { Message } from "node-nats-streaming"
 
 const setup = async () =>{
    const listener = new OrderCreatedListener(natsWrapper.client)
@@ -9,6 +10,19 @@ const setup = async () =>{
    const data: OrderCreatedEvent['data'] = {
       id: mongoose.Types.ObjectId().toHexString(),
       version: 0,
-      expiresAt 
+      expiresAt: 'fasdrfq',
+      userId: 'd21321',
+      status: OrderStatus.Created,
+      ticket:{
+         id: 'safwe',
+         price: 10
+      } 
    }
+
+   // @ts-ignore
+   const msg: Message ={
+      ack: jest.fn()
+   }
+
+   return {listener, data, msg}
 }
