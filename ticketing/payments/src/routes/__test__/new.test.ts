2a54cd72dd698450bd687e65d4a2c1793367f686
LaupWing <loc_strict@hotmail.com>
2021-06-15 16:58:23 +0200

    450

--- ticketing/payments/src/routes/__test__/new.test.ts
+++ ticketing/payments/src/routes/__test__/new.test.ts
@@ -5,7 +5,6 @@ import { app } from '../../app'
 import { Order } from '../../models/Order'
 import { stripe } from '../../stripe'
 
-jest.mock('../../stripe')
 
 it('returns a 404 when purchasing an order that not exists', async()=>{
    await request(app)
@@ -79,9 +78,4 @@ it('returns a 204 withb valid inputs', async ()=>{
          orderId: order.id
       })
       .expect(201)
-
-   const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0]
-   expect(chargeOptions.source).toEqual('tok-visa')
-   expect(chargeOptions.amount).toEqual(20 * 100)
-   expect(chargeOptions.currency).toEqual('usd')
 })
