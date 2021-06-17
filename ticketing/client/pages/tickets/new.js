c8f9d1cb233d3a8ff84bada833eb4341046c99b5
LaupWing <loc_strict@hotmail.com>
2021-06-17 15:01:29 +0200

    463

--- ticketing/client/pages/tickets/new.js
+++ ticketing/client/pages/tickets/new.js
@@ -1,8 +1,18 @@
 import { useState } from "react"
+import useRequest from "../../hooks/use-request"
+import Router from 'next/router'
 
 const NewTicket = () =>{
    const [title, setTitle] = useState('')
    const [price, setPrice] = useState('')
+   const {doRequest, errors} = useRequest({
+      url: '/api/tickets',
+      method: 'post',
+      body:{
+         title, price
+      },
+      onSuccess: () => Router.push('/')
+   })
 
    const onBlur = () =>{
       const value = parseFloat(price)
@@ -14,10 +24,15 @@ const NewTicket = () =>{
       setPrice(value.toFixed(2))
    }
 
+   const onSubmit = (event) =>{
+      event.preventDefault();
+      doRequest()
+   }
+
    return (
       <div>
          <h1>Create a Ticket</h1>
-         <form action="">
+         <form onSubmit={onSubmit}>
             <div className="form-group">
                <label>Title</label>
                <input 
@@ -37,6 +52,7 @@ const NewTicket = () =>{
                   onChange={(e) => setPrice(e.target.value)}  
                />
             </div>
+            {errors}
             <button className="btn btn-primary">
                Submit
             </button>
