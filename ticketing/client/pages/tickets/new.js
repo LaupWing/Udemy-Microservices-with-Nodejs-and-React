df28c807f3b23da7561b232ecfe1e174ab96f4ec
LaupWing <loc_strict@hotmail.com>
2021-06-17 14:50:13 +0200

    461

--- ticketing/client/pages/tickets/new.js
+++ ticketing/client/pages/tickets/new.js
@@ -1,15 +1,41 @@
+import { useState } from "react"
+
 const NewTicket = () =>{
+   const [title, setTitle] = useState('')
+   const [price, setPrice] = useState('')
+
+   const onBlur = () =>{
+      const value = parseFloat(price)
+
+      if(isNaN(value)){
+         return
+      }
+
+      setPrice(value.toFixed(2))
+   }
+
    return (
       <div>
          <h1>Create a Ticket</h1>
          <form action="">
             <div className="form-group">
                <label>Title</label>
-               <input type="text" className="form-control"/>
+               <input 
+                  value={title} 
+                  type="text" 
+                  className="form-control"
+                  onChange={(e) => setTitle(e.target.value)}  
+                  />
             </div>
             <div className="form-group">
                <label>Title</label>
-               <input type="text" className="form-control"/>
+               <input 
+                  value={price} 
+                  type="text" 
+                  className="form-control"
+                  onBlur={onBlur}
+                  onChange={(e) => setPrice(e.target.value)}  
+               />
             </div>
             <button className="btn btn-primary">
                Submit
@@ -18,3 +44,5 @@ const NewTicket = () =>{
       </div>
    )
 }
+
+export default NewTicket
